1f13e1f4a44a7cd4665074c3b9879fdaa27e76f4
Santosh-Rajput <santoshrajput01sr@gmail.com>
2023-07-02 14:58:08 +0530

    update

--- src/components/canvas/index.js
+++
@@ -1,6 +0,0 @@
-import EarthCanvas from "./EarthCanvas";
-import BallCanvas from "./BallCanvas";
-import ComputersCanvas from "./ComputersCanvas";
-import StarsCanvas from "./StarsCanvas";
-
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
